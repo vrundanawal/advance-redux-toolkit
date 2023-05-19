c8e3fb1bc194ec4a9917268258a4ba1a6309fd03
Vrunda Nawal <nawal.vrunda@gmail.com>
2023-05-19 15:02:56 +0100

    removethe item from cart functionality

---
+++ store/cart-slice.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      //check if item is exist or not
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      //if the item is not existing
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        // existingItem.quantity = existingItem.quantity + 1;
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      //find the id into items state
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        //remove the id from items array
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
