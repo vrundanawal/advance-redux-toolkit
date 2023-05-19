5c1ab9242ee23ca82908be55da448c6bcf0983bd
Vrunda Nawal <nawal.vrunda@gmail.com>
2023-05-19 15:04:22 +0100

    Map the product items and display on Ui

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,40 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My first book",
+    description: "The first book I ever wrote",
+  },
+  {
+    id: "p2",
+    price: 16,
+    title: "My second book",
+    description: "The is amazing book",
+  },
+  {
+    id: "p3",
+    price: 10,
+    title: "My third book",
+    description: "The is new book",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -25,7 +25,19 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() {},
+
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      //find the id into items state
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        //remove the id from items array
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
 
